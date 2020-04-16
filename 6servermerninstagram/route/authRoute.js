73a5474b2a75c38f17e4bd4b7ce5d1d091b8761e
walid <gougamwalid@gmail.com>
2020-04-16 18:51:39 +0200

    initialization of jsonwebtoken

--- 6servermerninstagram/route/authRoute.js
+++ 6servermerninstagram/route/authRoute.js
@@ -1,38 +1,82 @@
-const express = require("express");
-const router = express.Router();
-const user = require("../model/user")
+const express = require('express')
+const router = express.Router()
+const user = require('../model/user')
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const dotenv= require("dotenv")
 
-router.get("/", (req, res) => {
-    res.send("ca marche")
+dotenv.config({
+    path: './config/config.env'
+  })
+
+  const {JWT_SECRET_KEY} = process.env;
+
+
+router.get('/', (req, res) => {
+  res.send('ca marche')
 })
 
-router.post("/signup", (req, res) => {
-    const {
-        name,
-        email,
-        password
-    } = req.body;
-    if (!name || !email || !password) {
-        res.status(404).json({
-            error: "please fill all field"
-        })
-    } else {
-        res.status(200).json({
-            name,
-            email,
-            password
+router.post('/signup', (req, res) => {
+  const { name, email, password } = req.body
+  if (!name || !email || !password) {
+    res.status(404).json({
+      error: 'please fill all field'
+    })
+  }
+  user
+    .findOne({ email: email })
+    .then(savedUser => {
+      if (savedUser) {
+        res.send('already registred')
+      } else {
+          bcrypt.hash(password,12).then(hashed=>{
+            let User = new user({
+                name,
+                email,
+                password:hashed
+              })
+              let myUser = new Promise((resolve, reject) => {
+                resolve(User.save())
+              })
+              myUser
+                .then(e => console.log(e, 'result'))
+                .catch(err => console.log(err))
+
+                res.send("tout est ok")
+          }).catch(err=>console.log(err, "password not hashed"))
+       
+      }
+    })
+    .catch(err => console.log(err, "error when saved user"))
+})
+
+router.post("/signin", (req,res)=>{
+    const {email,password} = req.body; 
+    if(!email||!password){
+        res.json({
+            error:"you have to fill all field"
         })
     }
-
-    const User = new user({
-        name,
-        email,
-        password
-    })
-
-    User.save().then(resu => res.send(resu)).catch(err => res.send(err))
-
-
+    user.findOne({email:email}).then((savedUser)=>{
+        if(savedUser){
+            const token = jwt.sign({_id:savedUser._id},JWT_SECRET_KEY)
+            bcrypt.compare(password,savedUser.password).then(goodPassword=>{
+                console.log(token,"token")
+                if(goodPassword){
+                    res.json({
+                        message: "you are the good user you can connect"
+                    })
+                }
+                else{
+                    res.status(404).json({
+                        message: " wrong name or password"
+                    })
+                }
+            })
+        }
+    }
+    ).catch(err=>console.log(err,"erreur dans la verification pour savoir si le user a deja ete saved ou non"))
 })
+
 
 module.exports = router
--- 6servermerninstagram/server.js
+++ 6servermerninstagram/server.js
@@ -1,20 +1,18 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const authRoute = require("./route/authRoute")
-const connectDB = require("./config/db")
+const express = require('express')
+const dotenv = require('dotenv')
+const authRoute = require('./route/authRoute')
+const connectDB = require('./config/db')
 
 dotenv.config({
-    path: "./config/config.env"
+  path: './config/config.env'
 })
 
 connectDB()
-const app = express();
+const app = express()
 
 app.use(express.json())
-app.use('/', authRoute);
+app.use('/', authRoute)
 
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT)
